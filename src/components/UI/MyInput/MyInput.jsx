f0a5492834cf73fc193b4f53a6723e622ae438d3
GerasimovArtemiy <artyomgerasim@yandex.ru>
2022-02-24 05:01:47 +0300

    v-02 not adaptive complete

---
+++ src/components/UI/MyInput/MyInput.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import './MyInput.scss';
+
+export default function MyInput({ label, placeholder, type }) {
+    return (
+        <div className="orderpage__step-1_search">
+            <label htmlFor="search">{label}</label>
+            <input id="search" placeholder={placeholder} type={type} />
+        </div>
+    );
+}
+MyInput.propTypes = {
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    type: PropTypes.string,
+};
+MyInput.defaultProps = {
+    label: 'Город',
+    placeholder: 'Начните вводить...',
+    type: 'text',
+};
--- src/pages/OrderPage1/OrderPage1.jsx
+++ src/pages/OrderPage1/OrderPage1.jsx
@@ -1,23 +1,26 @@
+import MyInput from '../../components/UI/MyInput/MyInput';
 import './OrderPage1.scss';
+import googleMap from '../../assets/img/google-map.jpg';
 
 export default function OrderPage1() {
     return (
-        <div className="orderpage__step_1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum est et dicta saepe veniam
-            consectetur dignissimos libero, laudantium sed molestias. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quidem officiis ipsam mollitia labore temporibus nobis
-            architecto natus soluta numquam minus!OrderPage22222Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Earum est et dicta saepe veniam consectetur dignissimos
-            libero, laudantium sed molestias. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Quidem officiis ipsam mollitia labore temporibus nobis architecto natus soluta
-            numquam minus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum est et
-            dicta saepe veniam consectetur dignissimos libero, laudantium sed molestias. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quidem officiis ipsam mollitia labore
-            temporibus nobis architecto natus soluta numquam minus!Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Earum est et dicta saepe veniam consectetur dignissimos
-            libero, laudantium sed molestias. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Quidem officiis ipsam mollitia labore temporibus nobis architecto natus soluta
-            numquam minus!
+        <div className="orderpage__step-1">
+            <div className="orderpage__step-1_searches">
+                <MyInput
+                    label="Город"
+                    type="search"
+                    placeholder="Начните вводить город ..."
+                />
+                <MyInput
+                    label="Пункт выдачи"
+                    type="search"
+                    placeholder="Начните вводить пункт ..."
+                />
+            </div>
+            <div className="orderpage__step-1_map">
+                <h2>Выбрать на карте:</h2>
+                <img src={googleMap} alt="map" />
+            </div>
         </div>
     );
 }
