818b6aa3a5c2ad1fc86369fafb232f6e0d8de1c1
GerasimovArtemiy <artyomgerasim@yandex.ru>
2022-03-18 03:10:22 +0300

    fix errors f-03

--- src/components/Ymap/Ymap.jsx
+++ src/components/Ymap/Ymap.jsx
@@ -1,46 +1,75 @@
 import { useEffect, useState } from 'react';
-// import { useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 import { yandexMapKey } from '../../API/constants';
+import { setPoint } from '../../store/Slices/AllFormSlice';
 import placemark from '../../assets/icons/placemark.svg';
 import './Ymap.scss';
 
-export default function Ymap({ city, point }) {
+export default function Ymap() {
+    const dispatch = useDispatch();
+    const { city, point } = useSelector((state) => state.allForm);
+    const { fetchCityPoints } = useSelector((state) => state.fetchLocation);
     const [ymap, setYmap] = useState(null);
     const [zoom, setZoom] = useState(12);
     const [center, setCenter] = useState([54.31, 48.39]);
-    const stateForMap = {
-        center,
-        zoom,
-    };
+    const [coords, setCoords] = useState([]);
     const optionsForPlacemark = {
         iconLayout: 'default#image',
         iconImageSize: [16, 16],
         iconImageHref: placemark,
     };
-
-    async function getGeolocation() {
-        if (point.name) {
-            const geocoder = await ymap.geocode(`${city.name} ${point.name}`);
-            const coords = geocoder.geoObjects.get(0).geometry.getCoordinates();
-            setCenter(coords);
-        }
-    }
-    function changeZoom() {
-        if (point.name) {
-            setZoom(16);
-        } else {
-            setZoom(12);
-        }
-    }
+    const stateForMap = {
+        center,
+        zoom,
+    };
     function getGeocoder(ymaps) {
         setYmap(ymaps);
     }
+
+    async function getGeolocation(address) {
+        if (ymap) {
+            const geocoder = await ymap.geocode(address);
+            const pointLocation = geocoder.geoObjects.get(0).geometry.getCoordinates();
+            return pointLocation;
+        }
+        return null;
+    }
+    async function getPointsMap(points) {
+        const geoloacationPoints = [];
+        for (const item of points) {
+            const loacation = await getGeolocation(`${city.name},${item.address}`);
+            geoloacationPoints.push({ coordinates: loacation, point: item });
+        }
+        setCoords(geoloacationPoints);
+    }
+    async function changeCenterState(address, isCity = false) {
+        if (isCity) {
+            setZoom(12);
+        } else {
+            setZoom(16);
+        }
+        const pointLocation = await getGeolocation(address);
+        setCenter(pointLocation);
+    }
+
     useEffect(() => {
-        getGeolocation();
-        changeZoom();
-    }, [point]);
+        if (fetchCityPoints.data) {
+            getPointsMap(fetchCityPoints.data);
+        }
+        if (city.name) {
+            changeCenterState(city.name, true);
+        }
+    }, [fetchCityPoints.data]);
+
+    useEffect(() => {
+        if (!point.name && city.name) {
+            changeCenterState(city.name, true);
+        }
+        if (point.name) {
+            changeCenterState(`${city.name},${point.name}`);
+        }
+    }, [point.name]);
 
     return (
         <div>
@@ -53,18 +82,25 @@ export default function Ymap({ city, point }) {
             >
                 <div>
                     <Map className="yandexmap__map" state={stateForMap} onLoad={(ymaps) => getGeocoder(ymaps)}>
-                        <Placemark geometry={point.name ? center : ['', '']} options={optionsForPlacemark} />
+                        {coords &&
+                            coords?.map((coordinate, index) => (
+                                <Placemark
+                                    key={index}
+                                    geometry={coordinate.coordinates}
+                                    options={optionsForPlacemark}
+                                    onClick={() => {
+                                        dispatch(
+                                            setPoint({
+                                                name: coordinate.point.address,
+                                                id: coordinate.point.id,
+                                            })
+                                        );
+                                    }}
+                                />
+                            ))}
                     </Map>
                 </div>
             </YMaps>
         </div>
     );
 }
-Ymap.propTypes = {
-    city: PropTypes.instanceOf(Object),
-    point: PropTypes.instanceOf(Object),
-};
-Ymap.defaultProps = {
-    city: {},
-    point: {},
-};
--- src/store/Slices/FetchLocationSlice.js
+++ src/store/Slices/FetchLocationSlice.js
@@ -20,6 +20,10 @@ export const getCityPoints = createAsyncThunk('fetchCityPoints/getCityPoints', a
     const response = await FetchAPI.getCityPoints(cityId);
     return response.data.data;
 });
+export const getAllPoints = createAsyncThunk('fetchAllPoints/getAllPoints', async () => {
+    const response = await FetchAPI.getAllPoints();
+    return response.data.data;
+});
 
 const FetchLocationSlice = createSlice({
     name: 'fetchLocation',
@@ -57,6 +61,21 @@ const FetchLocationSlice = createSlice({
             }
         });
         builder.addCase(getCityPoints.rejected, (state) => {
+            state.fetchCityPoints.status = 'rejected';
+        });
+
+        builder.addCase(getAllPoints.pending, (state) => {
+            state.fetchCityPoints.status = 'loading';
+        });
+        builder.addCase(getAllPoints.fulfilled, (state, action) => {
+            if (action.payload) {
+                state.fetchCityPoints.data = action.payload;
+                state.fetchCityPoints.status = 'resolved';
+            } else {
+                state.fetchCityPoints.status = 'rejected';
+            }
+        });
+        builder.addCase(getAllPoints.rejected, (state) => {
             state.fetchCityPoints.status = 'rejected';
         });
     },
