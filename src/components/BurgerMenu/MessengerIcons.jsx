a01199ee95626faa26fed25a6ebad950fe21c435
GerasimovArtemiy <artyomgerasim@yandex.ru>
2022-03-28 13:28:02 +0300

    delete META icons

--- src/components/BurgerMenu/MessengerIcons.jsx
+++ src/components/BurgerMenu/MessengerIcons.jsx
@@ -1,13 +1,11 @@
-import { ReactComponent as FacebookIcon } from '../../assets/icons/MessengerIcons/facebook_icon.svg';
-import { ReactComponent as InstagramIcon } from '../../assets/icons/MessengerIcons/instagram_icon.svg';
 import { ReactComponent as TelegramIcon } from '../../assets/icons/MessengerIcons/telegram_icon.svg';
 
 export default function MessengerIcons() {
     return (
         <div className="burger__menu_icon">
             <TelegramIcon className="burger__menu_icon-telegram" />
-            <FacebookIcon className="burger__menu_icon-facebook" />
-            <InstagramIcon className="burger__menu_icon-instagram" />
+            <TelegramIcon className="burger__menu_icon-telegram" />
+            <TelegramIcon className="burger__menu_icon-telegram" />
         </div>
     );
 }
